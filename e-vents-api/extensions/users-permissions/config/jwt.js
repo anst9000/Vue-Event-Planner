975a7e2cc810df2edc7fe27b8138ad8762e2416a
anst9000 <coltlaboy@yahoo.se>
2020-10-09 13:04:10 +0200

    first commit

---
+++ e-vents-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '322ebf3a-7452-4b98-95db-4cdcfa9a3864'
+};
---
+++ e-vents-front/src/templates/Event.vue
@@ -0,0 +1,33 @@
+<template>
+  <Layout>
+    <h1>{{ $page.event.title }}</h1>
+    <img :src="`http://localhost:1337${$page.event.image}`" />
+    <p>{{ $page.event.description }}</p>
+  </Layout>
+</template>
+
+<page-query>
+query($id: ID!) {
+	event(id: $id) {
+    id
+    title
+    duration
+    price
+    date
+    image
+    description
+    image
+  }
+}
+
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.event.title,
+    };
+  },
+};
+</script>
